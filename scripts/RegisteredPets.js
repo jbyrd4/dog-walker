7c68a29b6cf669beac163dc3959124df1695c20d
jbyrd4 <jbyrd4@gmail.com>
2021-12-01 10:47:01 -0600

    ch3 explorer1 Complete

--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -1,12 +1,14 @@
 import { getPets } from "./database.js"
+import { getWalkers } from "./database.js"
 
 const pets = getPets()
+const walkers = getWalkers()
 
 export const RegisteredPets = () => {
     let petHTML = "<ul>"
 
     for (const pet of pets) {
-        petHTML += `<li>${pet.name}</li>`
+        petHTML += `<li id="pet--${pet.id}"">${pet.name}</li>`
     }
 
     petHTML += "</ul>"
@@ -14,3 +16,32 @@ export const RegisteredPets = () => {
     return petHTML
 }
 
+// Event Listener With Nested For Loop
+//
+// document.addEventListener("click", (clickEvent) => {
+//     const itemClicked = clickEvent.target
+//     if (itemClicked.id.startsWith("pet")) {
+//         const [,petId] = itemClicked.id.split("--")
+//         for (const pet of pets) {
+//             if (pet.id === parseInt(petId)) {
+//                 for (const walker of walkers) {
+//                     if (walker.id === pet.walkerId)
+//                     window.alert(`${pet.name} is being walked by ${walker.name}.`)
+//                 }
+//             }
+//         }
+        
+//     }
+// })
+
+// Event Listener With Find
+//
+document.addEventListener("click", (clickEvent) => {
+    const itemClicked = clickEvent.target
+    if (itemClicked.id.startsWith("pet")) {
+        const [,petId] = itemClicked.id.split("--")
+        const petClicked = pets.find((pet) => parseInt(petId) === pet.id)
+        const petWalker = walkers.find((walker) => walker.id === petClicked.walkerId)
+        window.alert(`${petClicked.name} is being walked by ${petWalker.name}`)
+    }
+})
